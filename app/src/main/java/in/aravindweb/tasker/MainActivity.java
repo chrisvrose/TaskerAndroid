c460ec318eef7fcf8ea2a75f99996722b436496a
Christopher Rose <christophervrose@gmail.com>
2021-06-30 22:33:19 +0530

    [pain2]

--- app/src/main/java/in/aravindweb/tasker/MainActivity.java
+++ app/src/main/java/in/aravindweb/tasker/MainActivity.java
@@ -15,8 +15,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private AppBarConfiguration mAppBarConfiguration;
@@ -24,12 +31,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavController navController;
+//    private TextView x=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -43,6 +52,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navigationView.setNavigationItemSelectedListener(this);
 
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.tokenLocation), Context.MODE_PRIVATE);
+
+        String token = sharedPref.getString("token","-");
+        //set profile
+        AndroidNetworking.get("https://tasker.aravindweb.in/api/auth").addHeaders("X-Auth-Token",token).build().getAsJSONObject(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject res) {
+//                navigationView.getHeaderView(0
+                Log.d("res.output",res.toString());
+                try {
+                    String name = res.getString("name");
+                    String email = res.getString("email");
+                    View v = (navigationView.getHeaderView(0));
+                    ((TextView)v.findViewById(R.id.navbarEmail)).setText(email);
+                    ((TextView)v.findViewById(R.id.navbarDisplayName)).setText(name);
+                    ((TextView)v.findViewById(R.id.navbarEmailImage)).setText(name.substring(0,1));
+
+                } catch (JSONException e) {
+                    Log.d("res.output.output",res.toString());
+
+                    e.printStackTrace();
+                }
+//                String isTeacher = res.getString("email");
+
+            }
+
+            @Override
+            public void onError(ANError anError) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ((TextView)navigationView.getHeaderView(0).findViewById(R.id.navbarEmail)).setText("Bopooo");
+//         x = (TextView) findViewById(R.id.navbarDisplayName);
+//                x.setText("NOOOOOOOO");
     }
 
     public void logout(){
