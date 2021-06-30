107102221c25ef250c08c96b04c85bce041d9d61
Christopher Rose <christophervrose@gmail.com>
2021-06-30 20:56:01 +0530

    [pain]

--- app/src/main/java/in/aravindweb/tasker/MainActivity.java
+++ app/src/main/java/in/aravindweb/tasker/MainActivity.java
@@ -1,23 +1,52 @@
 package in.aravindweb.tasker;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private AppBarConfiguration mAppBarConfiguration;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        this.drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home /*R.id.nav_messages*//*, R.id.nav_admin*/)
+                .setDrawerLayout(drawerLayout)
+                .build();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
-    public void logout(View v){
+    public void logout(){
         SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.tokenLocation),Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.remove("token");
@@ -28,5 +57,19 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.nav_logout:
+                logout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
